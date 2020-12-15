2024bad370e0a50cc6127e8f266ed402b8ed012d
Hasan-98 <59702148+Hasan-98@users.noreply.github.com>
2020-12-15 23:41:51 +0500

    awdf

--- app/src/main/java/com/example/docport/MainActivity.java
+++ app/src/main/java/com/example/docport/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.docport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button createAccount , Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
+
+        createAccount = (Button)findViewById(R.id.createAccount);
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent i = new Intent(MainActivity.this , userRegistration.class);
+               startActivity(i);
+            }
+        });
+        Login = (Button)findViewById(R.id.login);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ii = new Intent(MainActivity.this , Landing.class );
+                startActivity(ii);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/example/docport/userRegistration.java
@@ -0,0 +1,13 @@
+package com.example.docport;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class userRegistration extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.useregistration);
+    }
+}
